651200dde291c6e67f2ad5bb6cd81ae7a71f7a66
firsy <156234443+firsy0@users.noreply.github.com>
2024-01-29 19:32:55 +0300

    0001

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("Hello world")
+numOne = 10
+numTwo = 15
+
+#print("x + y = ", numOne + numTwo)
+#print("x - y = ", numOne - numTwo)
+#print("x / y = ", numOne / numTwo)
+#print("x * y = ", numOne * numTwo)
+#print("x ** y = ", numOne ** numTwo)
+#print("x % y = ", numOne % numTwo)
+
+#input
+
+firstName = str(input("Please enter your firstname: ")).capitalize()
+lastName = str(input("Please enter your lastname: ")).capitalize()
+ftName = str(input("Please enter your fathersname: ")).capitalize()
+
+print(lastName + " " + firstName[0] + ". " + ftName[0] + ". ")
