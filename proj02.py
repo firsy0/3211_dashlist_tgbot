dfa584e554a5349f755050645a4dfddeda39f2ec
firsy <156234443+firsy0@users.noreply.github.com>
2024-01-29 20:50:59 +0300

    0002

---
+++ proj02.py
@@ -0,0 +1,58 @@
+#numOne = 10
+#numTwo = 15
+
+#print("x + y = ", numOne + numTwo)
+#print("x - y = ", numOne - numTwo)
+#print("x / y = ", numOne / numTwo)
+#print("x * y = ", numOne * numTwo)
+#print("x ** y = ", numOne ** numTwo)
+#print("x % y = ", numOne % numTwo)
+
+fname = input("Your first name: ").capitalize()
+lname = input("Your last name: ").capitalize()
+age = input("Your age: ")
+phoneNum = input("Your phone number: ")
+
+print()
+
+print("......,^~íí~^,......")
+print("...^íííííííííííí^...")
+print(".,ííííí~´..´~ííííí,.")
+print(",ííííí'......'ííííí,")
+print("~ííííí^......^ííííí~")
+print("íííííííí'´´'íííííííi")
+print("^íííí^''''''''^íííí^")
+print(".ííí............ííí.")
+print("..^í^..........'í^..")
+print("....,ííí^^^^ííí,....")
+
+print()
+
+print("First name:", fname)
+print("Last name:", lname)
+print("Age:", age)
+print("Phone number:", phoneNum)
+
+print()
+
+print(".........db.........")
+print("........d00b........")
+print(".......d0000b.......")
+print("......d000000b......")
+print(".....d00000000b.....")
+print(".....#--------#.....")
+print(".....#-______-#.....")
+print(".....#-|" + lname[0] + "." + fname[0] + "." + "|-#.....")
+print(".....#-L____J-#.....")
+print(".....#--------#.....")
+print("....d#--------#b....")
+print("...d.#--------#.b...")
+print("..d..##########..b..")
+print(".d000#-0____0-#000b.")
+print("......0______0......")
+print("...../p______q\.....")
+print(".....|________|.....")
+
+print()
+
+print("Programming languages: Ruby, Java, C++, C#, C, Pascal, Swift, PHP, Rust, Assembly")
